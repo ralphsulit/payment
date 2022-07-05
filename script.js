fb0b7cbb012c4f4ecbda73082edda8c7bc5cb2a1
Joseph <ralphlsulit@gmail.com>
2022-07-05 11:17:15 +0800

    added function for billing address

---
+++ script.js
@@ -0,0 +1,34 @@
+// Variables
+let submitBtn = document.getElementById('billingBtn');
+let email = document.getElementById('billingEmail');
+let fullName = document.getElementById('billname');
+let address = document.getElementById('billAddress');
+let phoneNum = document.getElementById('billPhoneNumber');
+
+// Billing Info Variables
+let infoName = document.getElementById('infoName');
+let infoAddress = document.getElementById('infoAddress');
+let infoNumber = document.getElementById('infoNumber');
+let infoEmail = document.getElementById('infoEmail');
+
+
+// Edit Billing Information
+submitBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  
+  // FullName
+  infoName.innerText = fullName.value;
+  fullName.value = '';  
+
+  // Address
+  infoAddress.innerText = address.value;
+  address.value = '';
+
+  // Phone Number 
+  infoNumber.innerText = phoneNum.value;
+  phoneNum.value = '';
+
+  //email
+  infoEmail.innerText = email.value;
+  email.value = '';
+})
